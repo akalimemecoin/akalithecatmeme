fd605444bea93592874bcd6f397c04b45a7b6e81
akalimemecoin <jacgcmain@gmail.com>
2025-02-10 06:52:19 +0100

    final

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-window.addEventListener('load', () => {
-    const catImage = document.querySelector('.image-container img');
-    
-    // Apply scaling effect on page load
-    catImage.style.transform = 'scale(2.8)';
-    catImage.style.transition = 'transform 1s ease';
-
-    // Revert to normal size after 1 second
-    setTimeout(() => {
-        catImage.style.transform = '';
-    }, 1000);
-});
-=======
 window.addEventListener('load', () => {
     const catImage = document.querySelector('.image-container img');
     
@@ -34,4 +20,4 @@ video.addEventListener("ended", () => {
 
 // Start playing when the page loads
 video.play();
->>>>>>> master
+
